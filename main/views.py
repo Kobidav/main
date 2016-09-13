73a9bc3c8866eca82a88fd4d0d2561f4784bc930
Jacob <davidson.j.kobi@gmail.com>
2016-09-13 09:50:15 +0300

    html modifite

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 def units(request):
-    return render(request, 'main/units.html', {})
+    all_units = Post.objects.order_by('published_date')
+    return render(request, 'main/units.html', {'all_units': all_units})
