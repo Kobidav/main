5ab8a6bd24854beb720f66057b478418b5288e40
Work Comp <davidson.j.kobi@gmail.com>
2016-11-07 12:43:37 +0200

    remove inv3 request

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.inv, name='inv'),
-    url(r'^warr$', views.warr, name='warr'),
-    url(r'^inv3$', views.inv3, name='inv3'),
-    url(r'^mainn/(?P<ffname>\S+)/(?P<fvalue>\S+)/$', views.inv3, name='inv3'),
     url(r'^inv$', views.inv, name='inv'),
     url(r'^main/(?P<stype>\S+)/(?P<svalue>\S+)/(?P<lst_srt>\S+)/(?P<fsname>\S+)/(?P<shw_data>\S+)/$', views.sort_n, name='sort_n'),
 ]
