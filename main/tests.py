c769db44f142ea049708d87c7f2f760522fb2128
Kobi <davidson.j.kobi@gmail.com>
2017-01-19 09:25:21 +0200

    from ubunto selenium tests

--- main/tests.py
+++ main/tests.py
@@ -181,5 +181,24 @@ class MainPageTests(StaticLiveServerTestCase):
 
 
 
+class SeleniumTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(SeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(SeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        print (self.live_server_url)
+        self.selenium.get('%s%s' % (self.live_server_url, "/inv"))
+       # username_input = self.selenium.find_element_by_name("username")
+       # username_input.send_keys('myuser')
+       # password_input = self.selenium.find_element_by_name("password")
+       # password_input.send_keys('secret')
+       # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
 
